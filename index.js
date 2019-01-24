dc1feda9685b6a7191bb62e94c0ba3bbbcb349c7
Matthew Luvin <jmluvin@gmail.com>
2019-01-24 17:04:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var empty = [];
+  for (var i = 0; i < musicians.length; i++) {
+  var string = `${musicians[i]} plays ${instruments[i]}`;
+  empty.push(string);
+}
+return empty;
+}
+function johnLennonFacts(facts){
+var newfacts = [];
+let index = 0;
+  while (facts.length>index){
+    newfacts.push(facts[index]+"!!!");
+  index++;
+}
+return newfacts;
+}
+function iLoveTheBeatles(number) {
+  var love = [];
+  var i = 0;
+  do { love.push("I love the Beatles!");
+  i++;}
+  while (number < 15);
+  return love;
+}
